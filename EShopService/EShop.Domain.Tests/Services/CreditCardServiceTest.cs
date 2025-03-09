01a2ebaa426e91231e3b142071ee2e5dc56d8ba7
Bartłomiej Balsamski <bartlomiej.balsamski@gmail.com>
2025-03-09 21:06:30 +0100

    Lab 2 Ex. 2

---
+++ EShopService/EShop.Domain.Tests/Services/CreditCardServiceTest.cs
@@ -0,0 +1,50 @@
+﻿using EShop.Application.Services;
+
+namespace EShop.Application.Tests.Services
+{
+    public class CreditCardServiceTest
+    {
+        [Fact]
+        public void ValidateCard_CheckCardToShortLength_ReturnFalse()
+        {
+            // Arrange
+            var creditCardService = new CreditCardService();
+            string cardNumber = "1212";
+
+            // Act
+            var result = creditCardService.ValidateCardNumber(cardNumber);
+
+            // Assert
+            Assert.False(result);
+        }
+
+
+        [Fact]
+        public void ValidateCard_CheckCardCorrectLength_ReturnTrue()
+        {
+            // Arrange
+            var creditCardService = new CreditCardService();
+            string cardNumber = "349779658312797";
+
+            // Act
+            var result = creditCardService.ValidateCardNumber(cardNumber);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ValidateCard_CheckCardToLongLength_ReturnFalse()
+        {
+            // Arrange
+            var creditCardService = new CreditCardService();
+            string cardNumber = "349779658312797349779658312797";
+
+            // Act
+            var result = creditCardService.ValidateCardNumber(cardNumber);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
