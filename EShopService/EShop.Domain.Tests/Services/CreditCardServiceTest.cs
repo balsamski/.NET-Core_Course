68d4c9668cd7b898727a3bb7f1d6a3ae0eb6d486
Bartłomiej Balsamski <bartlomiej.balsamski@gmail.com>
2025-03-09 21:07:36 +0100

    Lab 2 Ex. 3

--- EShopService/EShop.Domain.Tests/Services/CreditCardServiceTest.cs
+++ EShopService/EShop.Domain.Tests/Services/CreditCardServiceTest.cs
@@ -46,5 +46,28 @@ namespace EShop.Application.Tests.Services
             // Assert
             Assert.False(result);
         }
+
+        [Theory]
+        [InlineData("3497 7965 8312 797")]
+        [InlineData("345-470-784-783-010")]
+        [InlineData("378523393817437")]
+        [InlineData("4024-0071-6540-1778")]
+        [InlineData("4532 2080 2150 4434")]
+        [InlineData("4532289052809181")]
+        [InlineData("5530016454538418")]
+        [InlineData("2551248451415297")]
+        [InlineData("2430688410640492")]
+        public void ValidateCard_CheckCardValidator_ReturnTrue(string cardNumber)
+        {
+            // Arrange
+            var creditCardService = new CreditCardService();
+
+            // Act
+            var result = creditCardService.ValidateCardNumber(cardNumber);
+
+            // Assert
+            Assert.True(result);
+        }
+
     }
 }
