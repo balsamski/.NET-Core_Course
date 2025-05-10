27ee6280661931cd448eb652363e4965958dcbe1
Bartłomiej Balsamski <bartlomiej.balsamski@gmail.com>
2025-05-10 09:26:38 +0200

    Lab 9 Ex. 2

--- EShopService/User.Application/Services/LoginService.cs
+++ EShopService/User.Application/Services/LoginService.cs
@@ -5,10 +5,12 @@ namespace User.Application.Services
     public class LoginService : ILoginService
     {
         protected IJwtTokenService _jwtTokenService;
+        protected Queue<int> _userLoggedIdsQueue;
 
         public LoginService(IJwtTokenService jwtTokenService)
         {
             _jwtTokenService = jwtTokenService;
+            _userLoggedIdsQueue = new Queue<int>();
         }
 
         public string Login(string username, string password)
@@ -17,6 +19,7 @@ namespace User.Application.Services
             {
                 var roles = new List<string> { "Client", "Employee", "Administrator" };
                 var token = _jwtTokenService.GenerateToken(123, roles);
+                _userLoggedIdsQueue.Enqueue(123);
                 return token;
             }else
             {
